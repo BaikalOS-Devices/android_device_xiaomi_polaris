b7b7410487e610263065b9c3a64367794973a439
andr68rus <ashtmb@gmail.com>
2019-01-13 11:26:57 +0300

    Baikal-fy

--- baikalos_polaris.mk
+++ baikalos_polaris.mk
@@ -4,13 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 $(call inherit-product, device/xiaomi/polaris/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/baikalos/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_polaris
+PRODUCT_NAME := baikalos_polaris
 PRODUCT_DEVICE := polaris
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi MIX 2S
@@ -24,3 +28,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE="polaris"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# BaikalOS Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Andrey Shidakov (andr68rus) and Sergey Vasiliev (sdv22)"
